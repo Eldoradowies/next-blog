69ca4a8f4c04e0b90d89d35773f998b129480782
Ahmad Abdullahi <wiesmanyaro@gmail.com>
2023-01-16 15:04:43 +0100

    updated

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ function Banner() {
   return (
     <div className="flex flex-col justify-between px-10 py-5 mb-10 font-bold lg:flex-row lg:space-x-5">
       <div>
-        <h1 className="text-7xl">Ahmad's Daily Blog</h1>
+        <h1 className="text-7xl">Ahmad Daily Blog</h1>
         <h2 className="mt-5 md:mt-0">
           Welcome to {` `}
           <span className="underline decoration-4 decoration-[#F7AB07]">
--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -13,25 +13,18 @@ function BlogList({posts }: Props) {
         <hr className="border-[#F7AB0A] mb-10" />
 
 
-        <div className="grid grid-cols-1 md:grid-cols-2 px-10 
-        gap-10 gap-y-16 pb-24">
+        <div className="grid grid-cols-1 gap-10 px-10 pb-24 md:grid-cols-2 gap-y-16">
           {posts.map(post => (
             <ClientSideRoute key={post._id} route={`/post/${post.slug.current}`}>
-              <div className="group cursor-pointer flex flex-col">
-                <div className="relative w-full h-80 drop-shadow-xl
-                group-hover:scale-105 transition-transform duration-200
-                ease-out
-                ">
+              <div className="flex flex-col cursor-pointer group">
+                <div className="relative w-full transition-transform duration-200 ease-out h-80 drop-shadow-xl group-hover:scale-105 ">
                   <Image 
                   className="object-cover object-left lg:object-center"
                   src={urlFor(post.mainImage).url()}
                   alt={post.author.name}
                   fill
                   />
-                  <div className="absolute bottom-0 w-full bg-opacity-20
-                  bg-black backdrop-blur-lg rounded drop-shadow-lg
-                  text-white p-5 flex justify-between
-                  ">
+                  <div className="absolute bottom-0 flex justify-between w-full p-5 text-white bg-black rounded bg-opacity-20 backdrop-blur-lg drop-shadow-lg ">
                     <div>
 
                       <p className="font-bold">
@@ -50,10 +43,9 @@ function BlogList({posts }: Props) {
                     
                     </div>
 
-                    <div className="flex flex-col md:flex-row gap-y-2
-                    md:gap-x-2 items-center
-                    ">
+                    <div className="flex flex-col items-center md:flex-row gap-y-2 md:gap-x-2 ">
                       {post.categories.map(category => (
+                        /* eslint-disable */
                         <div className="bg-[#F7AB0A] text-center text-black
                         px-3 py-1 rounded-full text-sm font-semibold
                         ">
@@ -64,13 +56,13 @@ function BlogList({posts }: Props) {
                   </div>
                 </div>
 
-                <div className="mt-5 flex-1 line-clamp-2">
-                  <p className="underline text-lg font-bold">{post.title}</p>
-                  <p className=" text-gray-500">{post.description}</p>
+                <div className="flex-1 mt-5 line-clamp-2">
+                  <p className="text-lg font-bold underline">{post.title}</p>
+                  <p className="text-gray-500 ">{post.description}</p>
                 </div>
 
-                      <p className="mt-5 font-bold flex items-center group-hover:underline">
-                        Read Post <ArrowUpRightIcon className="ml-2 h-4 w-4" />
+                      <p className="flex items-center mt-5 font-bold group-hover:underline">
+                        Read Post <ArrowUpRightIcon className="w-4 h-4 ml-2" />
                       </p>
               </div>
             </ClientSideRoute>
